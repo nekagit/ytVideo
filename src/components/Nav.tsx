cc27000f61c980ab4e90a993a99d650b4ae1ba5e
nekagit <nenad99@hotmail.de>
2023-12-23 13:50:24 +0100

    commit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,29 +1,52 @@
-import Logo from '../assets/images/logo.svg';
-import Search from '../assets/images/search.svg';
-import Store from '../assets/images/store.svg'
+import Logo from "../assets/images/logo.svg";
+import Search from "../assets/images/search.svg";
+import Store from "../assets/images/store.svg";
 
 function Nav() {
   return (
     <nav className="nav-wrapper">
-      <div className='nav-content' >
+      <div className="nav-content">
         <ul className="list-styled">
-            <li className='link-styled'>
-                <img src={Logo} alt=""  />
-            </li>
-            <li className='link-styled'>
-                Store
-            </li>
-            <li className='link-styled'>Mac</li>
-            <li className='link-styled'>Ipad</li>
-            <li>Iphone</li>
-            <li>Watch</li>
-            <li>AirPods</li>
-            <li>Tv & Home</li>
-            <li>Entertainment</li>
-            <li>Accessiors</li>
-            <li>Support</li>
-            <li><img src={Search} alt="" />Search</li>
-            <li><img src={Store} alt="" /> Store</li>
+          <li className="link-styled">
+            <img src={Logo} alt="" />
+          </li>
+          <li className="link-styled">
+            <a className="link-styled">Store</a>
+          </li>
+          <li className="link-styled">
+            <a className="link-styled">Mac</a>
+          </li>
+          <li>
+            <a className="link-styled">Ipad</a>
+          </li>
+          <li>
+            <a className="link-styled">Iphone</a>
+          </li>
+          <li>
+            <a className="link-styled">Watch</a>
+          </li>
+          <li>
+            <a className="link-styled">AirPods</a>
+          </li>
+          <li>
+            <a className="link-styled">Tv & Home</a>
+          </li>
+          <li>
+            <a className="link-styled">Entertainment</a>
+          </li>
+          <li>
+            <a className="link-styled">Accessiors</a>
+          </li>
+          <li>
+            <a className="link-styled">Support</a>
+          </li>
+          <li>
+            <img src={Search} alt="" />
+            Search
+          </li>
+          <li>
+            <img src={Store} alt="" /> Store
+          </li>
         </ul>
       </div>
     </nav>
